89a67af7989b2c7029edc1f546f98fff70117f07
iwagaki <iwagaki@users.sourceforge.net>
2013-07-20 03:09:46 +0900

    Fixed a bug that the script does not work with ADVANCED_OPTIMIZED

---
+++ ShinseiBankAutoLogin/private_.js
@@ -0,0 +1,16 @@
+var account_no = 'FILL_YOUR_ACCOUNT_NO';
+var account_pin = 'FILL_YOUR_ACCOUNT_PIN';
+var account_password = 'FILL_YOUR_ACCOUNT_PASSWORD';
+var account_table = {
+    //   0    1    2    3    4
+    'A': ['X', 'X', 'X', 'X', 'X'],
+    'B': ['X', 'X', 'X', 'X', 'X'],
+    'C': ['X', 'X', 'X', 'X', 'X'],
+    'D': ['X', 'X', 'X', 'X', 'X'],
+    'E': ['X', 'X', 'X', 'X', 'X'],
+    'F': ['X', 'X', 'X', 'X', 'X'],
+    'G': ['X', 'X', 'X', 'X', 'X'],
+    'H': ['X', 'X', 'X', 'X', 'X'],
+    'I': ['X', 'X', 'X', 'X', 'X'],
+    'J': ['X', 'X', 'X', 'X', 'X']
+};
